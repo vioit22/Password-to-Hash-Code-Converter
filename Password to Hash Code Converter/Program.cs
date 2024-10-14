d88180a9af8f33f1161b031c275ce4023244ef72
Vio IT <150695785+vioit22@users.noreply.github.com>
2024-10-14 22:44:43 +0300

    Add files via upload

---
+++ Password to Hash Code Converter/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Welcome to Password to Hash Code Converter. Click Enter to continue...");
+Console.ReadLine();
+string first = "";
+Console.WriteLine("Enter the password:");
+Console.ReadLine();
+Console.WriteLine(first.GetHashCode());
+Console.WriteLine("Please Run Again to convert another password. Click Enter to exit!");
+Console.ReadLine();
